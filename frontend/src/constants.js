fc91f738b8271de495190ff4928ba00e192c5361
ichraf khalfaoui <ichrafkhalfaoui1@gmail.cm>
2025-04-08 09:55:17 +0100

    update

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,8 @@
  export const BASE_URL =
-  process.env.NODE_ENV === ' development' ? 'http://localhost:5000' : 'https://artisano-urkx.onrender.com';
+  process.env.NODE_ENV === ' development' ? 'http://localhost:5000' : '';
+  //export const BASE_URL =process.env.NODE_ENV === ' production' ? 'https://artisano-urkx.onrender.com' : '';
+  
+  
 //export const BASE_URL = ''; // If using proxy
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
